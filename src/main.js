3baf7d8025897bcf4af5e38ac9a6c24683fe5528
adobe.shaflon@gmail.com <adobe.shaflon@gmail.com>
2020-03-30 22:57:49 +0900

    Google Analyticsを仕込んだ

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'G-N62D5H4SMJ'
+})
 
 new Vue({
   render: h => h(App),
